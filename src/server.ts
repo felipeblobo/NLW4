a36bb8d7588f5c44d5704e00e0b02e92d72b8083
Felipe Lobo <felipeblobo@gmail.com>
2021-02-25 00:07:47 -0300

    creating and peforming tests

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,6 @@
-import "reflect-metadata";
-import express from "express";
-import "./database";
-import { router } from "./router";
+import { app } from "./app";
 
-const app = express();
 const port = 3333;
-
-app.use(express.json());
-
-app.use(router);
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}!`);
